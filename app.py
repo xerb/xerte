121c3552ffdcc835f41f9c946a8a6fdbc4e0f9c9
J <jchristie1@gmail.com>
2019-01-09 16:17:45 -0700

    API endpoints created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request,
 
 
 app = Flask(__name__)
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return "Hot Dog!"
 
+@app.route("/start_job", methods=["POST"])
+def start_job():
+    return print(request)
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
