381252c0b8ec26c0e1ec78acf06c33a221de5373
Eric Streeper <eric@xerb.tv>
2019-01-14 13:06:20 -0700

    Get ffmpeg running

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from urllib import parse
+import subprocess
 
 
 app = Flask(__name__)
@@ -18,10 +19,19 @@ def start_job():
     return decode
 
 
-
 def urlencode(str):
     return parse.quote(str)
 
+
+def run_ffmpeg(url, options=None):
+    output_file = 'output.avi'
+    try:
+        subprocess.run(['ffmpeg', '-i', url, output_file], check=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        print('Calling ffmpeg failed')
+        return
+
+    return output_file
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
