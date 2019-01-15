a89ef23feeba16b2f4594922d9a52f23cff8304b
J Christie <jchristie1@gmail.com>
2019-01-15 13:06:12 -0700

    Two changes: index func goes to html template and transcoding seperated to a new file

--- app.py
+++ app.py
@@ -1,49 +1,17 @@
-from flask import Flask, request
-from urllib import parse
-import subprocess
-import threading
-import uuid
+from flask import Flask, render_template
+import transcode
 
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 
 @app.route("/")
 def index():
-    return "Hot Dog!"
+    return render_template('index.html')
 
 
 @app.route("/start_job", methods=["POST"])
 def start_job():
-    params = request.args
-    input_file_url = params['url']
-    decode = parse.unquote(input_file_url)
-    output_uuid = uuid.uuid4()
-    start_transcode_pipeline(decode, output_uuid)
-    return str(output_uuid), 201
-
-
-def urlencode(str):
-    return parse.quote(str)
-
-
-def start_transcode_pipeline(url, uuid):
-    ffmpeg_thread = threading.Thread(target=run_ffmpeg, args=(url, uuid))
-    ffmpeg_thread.start()
-
-
-def run_ffmpeg(url, uuid):
-    output_file = '{}.avi'.format(uuid)
-    try:
-        subprocess.run(
-                ['ffmpeg', '-i', url, output_file],
-                check=True, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError:
-        print('Calling ffmpeg failed')
-        return
-
-    return output_file
-
+   return transcode.start_tasks()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
