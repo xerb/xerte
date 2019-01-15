5794cdeda151674a9da5942e4a1edc8d7a75ae0f
J Christie <jchristie1@gmail.com>
2019-01-15 13:24:02 -0700

    Returned API code

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, request
+from urllib import parse
+import uuid
+
 import transcode
 
-app = Flask(__name__, template_folder='template')
-
-
-@app.route("/")
-def index():
-    return render_template('index.html')
+app = Flask(__name__)
 
 
 @app.route("/start_job", methods=["POST"])
 def start_job():
-   return transcode.start_tasks()
+    params = request.args
+    input_file_url = params['url']
+    decode = parse.unquote(input_file_url)
+    output_uuid = uuid.uuid4()
+    transcode.start_transcode_pipeline(decode, output_uuid)
+    return str(output_uuid), 201
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
