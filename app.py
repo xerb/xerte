47b320eb3271fa4a4bb99f76fc463499019e4a96
J Christie <jchristie1@gmail.com>
2019-01-19 20:11:41 -0700

    Job status route added and Doc strings.

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ app = Flask(__name__)
 
 @app.route("/start_job", methods=["POST"])
 def start_job():
+    """POST media file to be converted.
+    HTML decoding built in.
+    Responce will be a generated UUID which will correspond to created file name.
+    """
+
     params = request.args
     input_file_url = params['url']
     decode = parse.unquote(input_file_url)
@@ -16,5 +21,19 @@ def start_job():
     transcode.start_transcode_pipeline(decode, output_uuid)
     return str(output_uuid), 201
 
-if __name__ == "__main__":
+
+@app.route("/job_status", methods=["POST"]) # TODO Or a GET?
+def job_status():
+    """GET request with UUID to track if media is converted.
+    """
+    params = request.args
+    thread_name = params['uuid']
+    decode = parse.unquote(thread_name)
+    status = transcode.check_status(decode)
+
+
+    return status, 201 # TODO change this to the correct response 
+
+
+if __name__ == "__main__":V
     app.run(host='0.0.0.0', port=8080, debug=True)
